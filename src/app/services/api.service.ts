e83bddc8e95dc2bee8c196093d9a6b6fc858c0a9
devesh-uimatic <deveshkumar@uimatic.com>
2023-09-26 11:55:15 +0530

    change project name and removed email issue (%)

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -8,18 +8,18 @@ export class ApiService {
   constructor(private http: HttpClient) {}
 
   postUser(data: any) {
-    return this.http.post<any>('http://localhost:30005/userList/', data);
+    return this.http.post<any>('http://localhost:30004/userList/', data);
   }
 
   getUser() {
-    return this.http.get<any>('http://localhost:30005/userList/');
+    return this.http.get<any>('http://localhost:30004/userList/');
   }
 
   updateUser(data: any, id: number) {
-    return this.http.put<any>('http://localhost:30005/userList/' + id, data);
+    return this.http.put<any>('http://localhost:30004/userList/' + id, data);
   }
 
   deleteUser(id: number) {
-    return this.http.delete('http://localhost:30005/userList/' + id);
+    return this.http.delete('http://localhost:30004/userList/' + id);
   }
 }
